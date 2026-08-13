8a89b569ce79332c6f1eb2dc1514394aa8647b02
Rafał Całka <25438601+rafaucau@users.noreply.github.com>
2026-08-13 14:57:20 +0200

    chore: add migration for generating missing VAPID keys

---
+++ migrations/2026_08_13_create_missing_vapid_keys.php
@@ -0,0 +1,28 @@
+<?php
+
+use Flarum\Settings\SettingsRepositoryInterface;
+use Minishlink\WebPush\VAPID;
+
+return [
+    'up'=> function () {
+        $settings = resolve(SettingsRepositoryInterface::class);
+
+        if ($settings->get('fof-pwa.vapid.private') && $settings->get('fof-pwa.vapid.public')) {
+            return;
+        }
+
+        try{
+            $keys = VAPID::createVapidKeys();
+            $settings->set('fof-pwa.vapid.private', $keys['privateKey']);
+            $settings->set('fof-pwa.vapid.public', $keys['publicKey']);
+            $settings->set('fof-pwa.vapid.success', true);
+        } catch (\Throwable $e) {
+            $settings->set('fof-pwa.vapid.success', false);
+            $settings->set('fof-pwa.vapid.error', $e->getMessage());
+        }
+
+    },
+    'down'=> function () {
+        // nothing here
+    }
+];
