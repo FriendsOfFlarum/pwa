2d6a10aae8958733679d03fe1093ae5d4591037b
Rafał Całka <25438601+rafaucau@users.noreply.github.com>
2026-08-17 01:00:38 +0200

    fix tsconfig

--- js/sw/webpack.config.js
+++ js/sw/webpack.config.js
@@ -1,19 +1,17 @@
 const path = require('node:path');
 const baseConfig = require('flarum-webpack-config')();
 
-module.exports = (env, argv) => {
-  return {
-    name: 'sw',
-    mode: argv.mode || 'production',
-    target: 'webworker',
-    context: __dirname,
-    entry: './sw.ts',
-    output: {
-      filename: 'sw.js',
-      path: path.resolve(__dirname, '../dist'),
-      clean: false,
-    },
-    module: baseConfig.module,
-    resolve: baseConfig.resolve,
-  };
-};
+module.exports = (env, argv) => ({
+  name: 'sw',
+  mode: argv.mode || 'production',
+  target: 'webworker',
+  context: __dirname,
+  entry: './sw.ts',
+  output: {
+    filename: 'sw.js',
+    path: path.resolve(__dirname, '../dist'),
+    clean: false,
+  },
+  module: baseConfig.module,
+  resolve: baseConfig.resolve,
+});
