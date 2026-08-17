4a7486d9946444ead6b1a2d5d3228fc693be6c4a
Rafał Całka <25438601+rafaucau@users.noreply.github.com>
2026-08-17 02:02:30 +0200

    remove legacy hack

--- js/src/forum/helpers/isInStandaloneMode.ts
+++
@@ -1,2 +0,0 @@
-export default () =>
-  window.matchMedia('(display-mode: standalone)').matches || (window.navigator as any).standalone || document.referrer.includes('android-app://');
--- js/src/forum/index.tsx
+++ js/src/forum/index.tsx
@@ -1,28 +1,11 @@
 import app from 'flarum/forum/app';
-import { extend } from 'flarum/common/extend';
-import LinkButton from 'flarum/common/components/LinkButton';
-import SessionDropdown from 'flarum/forum/components/SessionDropdown';
 import addShareButtons from './addShareButtons';
 import addPushNotifications from './addPushNotifications';
-import ItemList from 'flarum/common/utils/ItemList';
-import isInStandaloneMode from './helpers/isInStandaloneMode';
-import type Mithril from 'mithril';
 import { registerServiceWorker } from './registerServiceWorker';
 
 app.initializers.add('fof-pwa', () => {
   app.beforeMount(() => {
     registerServiceWorker().catch((e) => console.error('SW registration failed', e));
-  });
-
-  extend(SessionDropdown.prototype, 'items', function (items: ItemList<Mithril.Children>) {
-    if (isInStandaloneMode() && items.has('administration')) {
-      items.setContent(
-        'administration',
-        <LinkButton icon="fas fa-wrench" href={app.forum.attribute<string>('adminUrl')} target="_self" external>
-          {app.translator.trans('core.forum.header.admin_button')}
-        </LinkButton>
-      );
-    }
   });
 
   addShareButtons();
