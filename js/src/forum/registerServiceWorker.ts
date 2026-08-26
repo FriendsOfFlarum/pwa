e14e275e8beb6c6b4f350d4d40acbd47c0c94efb
Rafał Całka <25438601+rafaucau@users.noreply.github.com>
2026-08-26 15:58:45 +0200

    refactor: simplify Service Worker

--- js/src/forum/registerServiceWorker.ts
+++ js/src/forum/registerServiceWorker.ts
@@ -1,25 +1,11 @@
 import app from 'flarum/forum/app';
-import { openDB, type IDBPDatabase } from 'idb';
 
-let dbPromise: Promise<IDBPDatabase> | null = null;
 let registrationPromise: Promise<ServiceWorkerRegistration | undefined> | undefined;
-
-const getDB = (): Promise<IDBPDatabase> => {
-  dbPromise ??= openDB('keyval-store', 1, {
-    upgrade(database) {
-      database.createObjectStore('keyval');
-    },
-  });
-
-  return dbPromise;
-};
 
 export async function registerServiceWorker(): Promise<ServiceWorkerRegistration | undefined> {
   if (!('serviceWorker' in navigator)) return undefined;
 
   const basePath = app.forum.attribute<string>('basePath').replace(/\/$/, '');
-  const database = await getDB();
-  await database.put('keyval', app.forum.data.attributes, 'flarum.forumPayload');
 
   await navigator.serviceWorker.register(`${basePath}/sw`, {
     scope: `${basePath}/`,
